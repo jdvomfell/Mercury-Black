693d3577adf2cc3b18dd2f11ceacf7dee2727664
Jesse David Vomfell <hogstomp@outlook.com>
2016-12-07 15:33:57 -0500

    Testing Windows and Set-Up SDL

--- Mercury-Black/Core.cpp
+++ Mercury-Black/Core.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <SDL.h>
 
-int main() {
+int main(int argc, char ** argv) {
 
+	SDL_Init(SDL_INIT_EVERYTHING);
 
+	SDL_Window * window;
 
+	window = SDL_CreateWindow("Project Mercury Black", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 400, SDL_WINDOW_RESIZABLE);
+
+	SDL_Delay(5000);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	return 0;
 }
